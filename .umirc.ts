e4fe6eca3ed46da0ecb0fb55762acaa7fb6d6458
wenhaoming <435203093@qq.com>
2020-12-28 00:08:02 +0800

    feat:路由问题

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,31 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: '浩明啦',
   mode: 'site',
-  locales:['zh-CN', '中文']
+  locales:['zh-CN', '中文'],
+  navs:[
+    {
+      title:'javascript',
+      children:[
+        {
+          title:'基础',
+          path:'/javascript/base'
+        },
+        {
+          title:'业务',
+          path:'/javascript/business'
+        }
+      ]
+    }
+  ],
+  menus:{
+    '/javascript/base':[{
+      title:'基础',
+      children:['/javascript/base']
+    }],
+    '/javascript/business':[{
+      title:'业务',
+      children:['/javascript/business']
+    }]
+  }
   // more config: https://d.umijs.org/config
 });
