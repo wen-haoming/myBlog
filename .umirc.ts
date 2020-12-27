eca14181a43deb4608756c6ee5223e96fb1155f1
wenhaoming <435203093@qq.com>
2020-12-27 23:13:46 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: '浩明啦',
+  mode: 'site',
+  locales:['zh-CN', '中文']
+  // more config: https://d.umijs.org/config
+});
---
+++ docs/getting-started.md
@@ -0,0 +1,14 @@
+---
+nav:
+  title: Getting Started
+  order: 1
+---
+
+# Getting Started
+
+Balabala...
+
+```diff
+- return a
++ let c = 1
+```
